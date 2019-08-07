4ab5ed132be6e90b752b2841d31f878df22e08c2
lakrids-mint <mettepernille@hotmail.com>
2019-08-07 17:44:08 +0200

    add frontend build

---
+++ build/precache-manifest.c548ea7e2c20232a0a7d8ac40dbb7c85.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd0e0d7179f437de6c15df241ab848b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b688d3e6cfb52e3f9eb",
+    "url": "/static/js/2.97e9b6ee.chunk.js"
+  },
+  {
+    "revision": "d14b71007550b497fbe5",
+    "url": "/static/js/main.9fb6bd33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
